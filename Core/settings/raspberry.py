4f2bdccc7e1473f4328e02c33eda1187b70f9bf1
DanMitroshin <alfocintavr@mail.ru>
2023-02-14 00:09:59 +0300

    Update valves settings to current device

--- Core/settings/raspberry.py
+++ Core/settings/raspberry.py
@@ -7,17 +7,26 @@ ACCURATE_VAKUMETR_PORT = 1
 ACCURATE_VAKUMETR_USB_PORT = '/dev/ttyUSB0'  # FOR CVD-GRAPHENE USE USB1 (?)
 CURRENT_SOURCE_PORT = 3
 
-VALVES_CONFIGURATION = [
-    {'PORT': 2, "NAME": "O_2", "IS_GAS": True},
+VALVES_CONFIGURATION_1 = [
+    {'PORT': 2, "NAME": "O_2", "IS_GAS": True, "INSTRUMENT_NUMBER": 1},
     {'PORT': 3, "NAME": "N_2", "IS_GAS": True},
     {'PORT': 4, "NAME": "Ar", "IS_GAS": True},
     {'PORT': 17, "NAME": "C_2", "IS_GAS": True},
     {'PORT': 6, "NAME": "F_2", "IS_GAS": True},
-    # {'PORT': 7, "NAME": "O_2", "IS_GAS": True},
-    # {'PORT': 8, "NAME": "N_2", "IS_GAS": True},
-    # {'PORT': 9, "NAME": "Ar", "IS_GAS": True},
-    # {'PORT': 10, "NAME": "C_2", "IS_GAS": True},
-    # {'PORT': 11, "NAME": "F_2", "IS_GAS": True},
+    # {'PORT': 12, "NAME": "PUMP", "IS_GAS": False},
+    # {'PORT': 13, "NAME": "AIR", "IS_GAS": False},
+]
+
+AIR_VALVE_CONFIGURATION = {
+    'PORT': 4,
+}
+
+VALVES_CONFIGURATION = [
+    {'PORT': 2, "NAME": "H_2", "IS_GAS": True, "INSTRUMENT_NUMBER": 2},
+    {'PORT': 3, "NAME": "Ar", "IS_GAS": True, "INSTRUMENT_NUMBER": 1},
+    # {'PORT': 4, "NAME": "Air", "IS_GAS": True},
+    {'PORT': 17, "NAME": "CH_4", "IS_GAS": True, "INSTRUMENT_NUMBER": 3},
+    # {'PORT': 6, "NAME": "F_2", "IS_GAS": True},
     # {'PORT': 12, "NAME": "PUMP", "IS_GAS": False},
     # {'PORT': 13, "NAME": "AIR", "IS_GAS": False},
 ]
