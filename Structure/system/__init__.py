ce23adfa7fbc75b19cc98b8c96f9c7b3aefef0d8
DanMitroshin <alfocintavr@mail.ru>
2023-02-14 23:12:47 +0300

    Add _determine_attributes function for ports of controllers

--- Structure/system/__init__.py
+++ Structure/system/__init__.py
@@ -4,6 +4,7 @@ from coregraphene.components.controllers import (
     ValveController,
     CurrentSourceController,
     RrgModbusController,
+    TermodatModbusController,
 )
 from coregraphene.system import BaseSystem
 from coregraphene.conf import settings
@@ -13,13 +14,44 @@ VALVES_CONFIGURATION = settings.VALVES_CONFIGURATION
 
 class CvdSystem(BaseSystem):
 
+    def _determine_attributes(self):
+        used_ports = []
+        self.vakumetr_port = None
+        self.rrg_port = None
+        self.termodat_port = None
+        attributes = [
+            ['vakumetr_port', AccurateVakumetrController],
+            ['rrg_port', RrgModbusController],
+            ['termodat_port', TermodatModbusController],
+        ]
+        for port_name, controller_class in attributes:
+            for port in ['/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2']:
+                if port in used_ports:
+                    continue
+                controller: AbstractController = controller_class(port=port)
+                controller.setup()
+                is_good = controller.check_command()
+                if is_good:
+                    setattr(self, port_name, port)
+                    used_ports.append(port)
+                    break
+
+        assert self.vakumetr_port is not None
+        assert self.rrg_port is not None
+        assert self.termodat_port is not None
+
     def _init_controllers(self):
-        self.accurate_vakumetr_controller = AccurateVakumetrController()
+        self.accurate_vakumetr_controller = AccurateVakumetrController(
+            port=self.vakumetr_port,
+        )
         self._valves = dict()
         self._rrgs = dict()
         for valve_conf in VALVES_CONFIGURATION:
             self._valves[valve_conf["NAME"]] = ValveController(port=valve_conf["PORT"])
-            # self._rrgs[valve_conf["NAME"]] = RrgModbusController(...)
+            self._rrgs[valve_conf["NAME"]] = RrgModbusController(
+                port=self.rrg_port,
+                instrument_number=valve_conf['INSTRUMENT_NUMBER'],
+            )
 
         self.current_source_controller = CurrentSourceController(
             on_change_current=self.on_change_current,
