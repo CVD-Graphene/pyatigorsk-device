352335df3a738e098de3fde88a4ea7470462be90
DanMitroshin <alfocintavr@mail.ru>
2023-02-23 22:18:34 +0300

    Move actions

---
+++ Core/settings/actions.py
@@ -0,0 +1,40 @@
+class ACTIONS_NAMES:
+    TURN_ON_PUMP = "TURN_ON_PUMP"
+    OPEN_VALVE = "OPEN_VALVE"
+    CLOSE_VALVE = "CLOSE_VALVE"
+    CLOSE_ALL_VALVES = "CLOSE_ALL_VALVES"
+    SET_RRG_VALUE = "SET_RRG_VALUE"
+    SET_RRG_VALUE_WITH_PAUSE = "SET_RRG_VALUE_WITH_PAUSE"
+    PUMP_OUT_CAMERA = "PUMP_OUT_CAMERA"
+    VENTILATE_CAMERA = "VENTILATE_CAMERA"
+
+    SET_TEMPERATURE = "SET_TEMPERATURE"
+    SET_TEMPERATURE_IN_TIME = "SET_TEMPERATURE_IN_TIME"
+    SET_TEMPERATURE_FOR_TERMODAT_DEVICE = "SET_TEMPERATURE_FOR_TERMODAT_DEVICE"
+    SET_SPEED_FOR_TERMODAT_DEVICE = "SET_SPEED_FOR_TERMODAT_DEVICE"
+
+    PAUSE = "PAUSE"
+    FULL_OPEN_PUMP = "FULL_OPEN_PUMP"
+    FULL_CLOSE_PUMP = "FULL_CLOSE_PUMP"
+    STABILIZE_PRESSURE = "STABILIZE_PRESSURE"
+
+
+class TABLE_ACTIONS_NAMES:
+    TURN_ON_PUMP = "Включить насос"
+    OPEN_VALVE = "Открыть клапан"
+    CLOSE_VALVE = "Закрыть клапан"
+    CLOSE_ALL_VALVES = "Закрыть все клапаны"
+    SET_RRG_VALUE = "Значение на РРГ"
+    SET_RRG_VALUE_WITH_PAUSE = "Значение на РРГ с паузой"
+    PUMP_OUT_CAMERA = "Откачать камеру"
+    VENTILATE_CAMERA = "Провентилировать камеру"
+
+    SET_TEMPERATURE = "Установить температуру"
+    SET_TEMPERATURE_IN_TIME = "Установить температуру за время"
+    SET_TEMPERATURE_FOR_TERMODAT_DEVICE = "Установить температуру (термодат)"
+    SET_SPEED_FOR_TERMODAT_DEVICE = "Установить скорость (термодат)"
+
+    PAUSE = "Пауза"
+    FULL_OPEN_PUMP = "Открыть клапан насоса"
+    FULL_CLOSE_PUMP = "Закрыть клапан насоса"
+    STABILIZE_PRESSURE = "Стабилизировать давление"
