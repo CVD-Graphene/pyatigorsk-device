e0d2d0f1fc746332eb3734af9910a4ffbd838877
DanMitroshin <alfocintavr@mail.ru>
2023-02-25 01:21:22 +0300

    Set actions for current project

--- Core/settings/actions.py
+++ Core/settings/actions.py
@@ -3,10 +3,21 @@ class ACTIONS_NAMES:
     OPEN_VALVE = "OPEN_VALVE"
     CLOSE_VALVE = "CLOSE_VALVE"
     CLOSE_ALL_VALVES = "CLOSE_ALL_VALVES"
+
+    FULL_OPEN_RRG = "FULL_OPEN_RRG"
+    FULL_CLOSE_RRG = "FULL_CLOSE_RRG"
     SET_RRG_VALUE = "SET_RRG_VALUE"
     SET_RRG_VALUE_WITH_PAUSE = "SET_RRG_VALUE_WITH_PAUSE"
+    SET_RRG_AND_KEEP_TO_PRESSURE = "SET_RRG_AND_KEEP_TO_PRESSURE"
+
     PUMP_OUT_CAMERA = "PUMP_OUT_CAMERA"
     VENTILATE_CAMERA = "VENTILATE_CAMERA"
+    SMALL_PUMP_OUT_CAMERA = "SMALL_PUMP_OUT_CAMERA"
+
+    TURN_ON_ALL_TERMODATS = "TURN_ON_ALL_TERMODATS"
+    TURN_OFF_ALL_TERMODATS = "TURN_OFF_ALL_TERMODATS"
+    SET_T_V_ALL_TERMODATS = "SET_T_V_ALL_TERMODATS"
+    WAIT_TARGET_TEMPERATURE = "WAIT_TARGET_TEMPERATURE"
 
     SET_TEMPERATURE = "SET_TEMPERATURE"
     SET_TEMPERATURE_IN_TIME = "SET_TEMPERATURE_IN_TIME"
@@ -24,10 +35,21 @@ class TABLE_ACTIONS_NAMES:
     OPEN_VALVE = "Открыть клапан"
     CLOSE_VALVE = "Закрыть клапан"
     CLOSE_ALL_VALVES = "Закрыть все клапаны"
+
+    FULL_OPEN_RRG = "Полностью открыть РРГ"
+    FULL_CLOSE_RRG = "Полностью закрыть РРГ"
     SET_RRG_VALUE = "Значение на РРГ"
     SET_RRG_VALUE_WITH_PAUSE = "Значение на РРГ с паузой"
+    SET_RRG_AND_KEEP_TO_PRESSURE = "Значение на РРГ на давление"
+
     PUMP_OUT_CAMERA = "Откачать камеру"
     VENTILATE_CAMERA = "Провентилировать камеру"
+    SMALL_PUMP_OUT_CAMERA = "Откачать камеру игольчатым вентилем"
+
+    TURN_ON_ALL_TERMODATS = "Включить все печки"
+    TURN_OFF_ALL_TERMODATS = "Выключить все печки"
+    SET_T_V_ALL_TERMODATS = "Установить T и V на печки"
+    WAIT_TARGET_TEMPERATURE = "Ждать установленной T печек"
 
     SET_TEMPERATURE = "Установить температуру"
     SET_TEMPERATURE_IN_TIME = "Установить температуру за время"
