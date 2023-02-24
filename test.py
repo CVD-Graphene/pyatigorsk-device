29078053512e99deb4894d78d699f11a03899948
DanMitroshin <alfocintavr@mail.ru>
2023-02-24 19:24:31 +0300

    Add simple call class test

--- test.py
+++ test.py
@@ -8,5 +8,20 @@ def test_init_local():
     system = CvdSystem(actions_list=[])
 
 
+def test_call_class():
+    class S(object):
+        index = 0
+
+        def __call__(self, *args, **kwargs):
+            print("CALL", self.index)
+            self.index += 1
+
+    s = S()
+    s()
+    s()
+    s()
+
+
 if __name__ == '__main__':
+    # test_call_class()
     test_init_local()
