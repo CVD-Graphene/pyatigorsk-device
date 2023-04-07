14a57d2e3eb869339e9ebd7890587159d553f35f
DanMitroshin <alfocintavr@mail.ru>
2023-04-08 00:24:40 +0300

    Move rrg flow action to coregraphene

--- Structure/system/system_actions/__init__.py
+++ Structure/system/system_actions/__init__.py
@@ -1,5 +1,3 @@
-import random
-
 from coregraphene.conf import settings
 from coregraphene.system_actions import (
     ManyDeviceSystemAction, ManyDeviceControllerAction,
@@ -78,17 +76,17 @@ class SetTemperatureAllTermodatsAction(SystemAction):
 # =========== CONTROLLER ACTIONS ================= #
 
 
-class GetCurrentFlowRrgControllerAction(ManyDeviceControllerAction):
-
-    def _on_get_value(self, value):
-        # print("CALL FUNC CURRENT FLOW CONTROLLER 2:", value)
-        self._controller.current_sccms[self._controller._last_thread_command.device_num] = value
-
-    def _call_function(self, value):
-        max_sccm = self._controller.get_max_sccm_device(
-            device_num=self._controller._last_thread_command.device_num)
-        if LOCAL_MODE:
-            value = random.random() * 100 * 100
-        value = float(value) / (100 * 100) * max_sccm
-        # print("CALL FUNC CURRENT FLOW CONTROLLER:", value)
-        return value
+# class GetCurrentFlowRrgControllerAction(ManyDeviceControllerAction):
+#
+#     def _on_get_value(self, value):
+#         # print("CALL FUNC CURRENT FLOW CONTROLLER 2:", value)
+#         self._controller.current_sccms[self._controller._last_thread_command.device_num] = value
+#
+#     def _call_function(self, value):
+#         max_sccm = self._controller.get_max_sccm_device(
+#             device_num=self._controller._last_thread_command.device_num)
+#         if LOCAL_MODE:
+#             value = random.random() * 100 * 100
+#         value = float(value) / (100 * 100) * max_sccm
+#         # print("CALL FUNC CURRENT FLOW CONTROLLER:", value)
+#         return value
