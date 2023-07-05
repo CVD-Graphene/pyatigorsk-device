7d465e3b1f51eacc946c2db3ba00ffa755bb30c7
DanMitroshin <alfocintavr@mail.ru>
2023-07-05 15:36:33 +0300

    Recipe actions update with new core api: sub actions and interrupting

--- Core/actions/__init__.py
+++ Core/actions/__init__.py
@@ -1,6 +1,6 @@
 import time
 
-from coregraphene.auto_actions import AppAction, IntKeyArgument, Argument, PositiveIntKeyArgument, \
+from coregraphene.actions import AppAction, IntKeyArgument, PositiveIntKeyArgument, \
     TimeEditArgument, GasListArgument, SccmArgument, ValveListArgument, PositiveFloatKeyArgument
 from coregraphene.conf import settings
 from coregraphene.recipe.exceptions import NotAchievingRecipeStepGoal
@@ -45,21 +45,6 @@ def get_total_seconds_from_time_arg(time_arg):
     return mins * 60 + secs
 
 
-class PauseAction(AppAction):
-    name = TABLE_ACTIONS_NAMES.PAUSE
-    key = ACTIONS_NAMES.PAUSE
-    args_info = [TimeEditArgument]
-
-    def action(self, sleep_time, *args, **kwargs):
-        seconds = get_total_seconds_from_time_arg(sleep_time)
-        # print("SLEEP SECONDS:", seconds, time.time())
-        start = time.time()
-        while time.time() - start < seconds:
-            time.sleep(1)
-            if self._is_stop_state():
-                break
-
-
 def get_gas_index_by_name(name):
     for i, gas in enumerate(settings.VALVES_CONFIGURATION):
         if gas['NAME'] == name:
@@ -72,15 +57,27 @@ def get_pump_index_by_name(name):
             return i
 
 
+class PauseAction(AppAction):
+    name = TABLE_ACTIONS_NAMES.PAUSE
+    key = ACTIONS_NAMES.PAUSE
+    args_info = [TimeEditArgument]
+
+    def do_action(self, seconds):  # , *args, **kwargs):
+        # print("SLEEP SECONDS:", seconds, time.time())
+        start = time.time()
+        while time.time() - start < seconds:
+            time.sleep(1)
+            self.interrupt_if_stop_state()
+
+
 class TurnOnAllTermodatsTableAction(AppAction):
     """
     Включить все печки (термодаты)
     """
     name = TABLE_ACTIONS_NAMES.TURN_ON_ALL_TERMODATS
     key = ACTIONS_NAMES.TURN_ON_ALL_TERMODATS
-    args_info = []
 
-    def action(self):
+    def do_action(self):
         self.system.turn_on_all_termodats_action()
 
 
@@ -90,9 +87,8 @@ class TurnOffAllTermodatsTableAction(AppAction):
     """
     name = TABLE_ACTIONS_NAMES.TURN_OFF_ALL_TERMODATS
     key = ACTIONS_NAMES.TURN_OFF_ALL_TERMODATS
-    args_info = []
 
-    def action(self):
+    def do_action(self):
         self.system.turn_off_all_termodats_action()
 
 
@@ -104,7 +100,7 @@ class SetTemperatureAndSpeedAllTermodatsTableAction(AppAction):
     key = ACTIONS_NAMES.SET_T_V_ALL_TERMODATS
     args_info = [PositiveFloatKeyArgument, TermodatSpeedArgument]
 
-    def action(self, temperature, speed):
+    def do_action(self, temperature, speed):
         self.system.set_temperature_and_speed_all_termodats_action(temperature, speed)
 
 
@@ -116,7 +112,7 @@ class FullOpenSingleRrgAction(AppAction):
     key = ACTIONS_NAMES.FULL_OPEN_RRG
     args_info = [GasListArgument]
 
-    def action(self, valve_name):
+    def do_action(self, valve_name):
         index = get_gas_index_by_name(valve_name)
         self.system.full_open_rrg_action(device_num=index)
 
@@ -129,7 +125,7 @@ class FullCloseSingleRrgAction(AppAction):
     key = ACTIONS_NAMES.FULL_CLOSE_RRG
     args_info = [GasListArgument]
 
-    def action(self, valve_name):
+    def do_action(self, valve_name):
         index = get_gas_index_by_name(valve_name)
         self.system.full_close_rrg_action(device_num=index)
 
@@ -142,8 +138,7 @@ class SetRrgSccmValueAction(AppAction):
     key = ACTIONS_NAMES.SET_RRG_VALUE
     args_info = [GasListArgument, SccmArgument]
 
-    def action(self, valve_name, sccm):
-        sccm = float(sccm)
+    def do_action(self, valve_name: str, sccm: float):
         index = get_gas_index_by_name(valve_name)
         self.system.set_target_rrg_sccm_action(sccm, device_num=index)
 
@@ -156,24 +151,23 @@ class SetRrgSccmAndKeepToPressureDeltaAction(AppAction):
     key = ACTIONS_NAMES.SET_RRG_AND_KEEP_TO_PRESSURE
     args_info = [GasListArgument, SccmArgument, PositiveFloatKeyArgument]
 
-    def action(self, valve_name, sccm, delta_pressure):
-        sccm = float(sccm)
-        delta_pressure = float(delta_pressure)
-
+    def do_action(self, valve_name: str, sccm: float, delta_pressure: float):
         index = get_gas_index_by_name(valve_name)
         self.system.set_target_rrg_sccm_action(sccm, device_num=index)
 
-        start_time = time.time()
         start_pressure = self.system.get_current_vakumetr_pressure()
         while self.system.get_current_vakumetr_pressure() - start_pressure < delta_pressure:
-            if self._is_stop_state():
-                break  # TODO: Or return here?
+            self.interrupt_if_stop_state()
+
             time.sleep(1)
-            if MAX_RECIPE_STEP_SECONDS and (time.time() - start_time >= MAX_RECIPE_STEP_SECONDS):
+            delta_time = time.time() - self.start_time
+            if MAX_RECIPE_STEP_SECONDS and (delta_time >= MAX_RECIPE_STEP_SECONDS):
+                # можно добавить пояснение
+                # self.system.add_error_log(f"Откачка не завершилась до достижения максимального времени")
                 raise NotAchievingRecipeStepGoal
 
         action_close_rrg = FullCloseSingleRrgAction()
-        action_close_rrg.set_functions(system=self.system,)
+        action_close_rrg.system = self.system
         action_close_rrg.action(valve_name)
 
 
@@ -182,17 +176,12 @@ class SetRrgSccmValueWithPauseAction(AppAction):
     key = ACTIONS_NAMES.SET_RRG_VALUE_WITH_PAUSE
     args_info = [GasListArgument, SccmArgument, TimeEditArgument]
 
-    def action(self, valve_name, sccm, time_pause):
-
-        action_rrg = SetRrgSccmValueAction()
-        action_rrg.set_functions(
-            system=self.system,
-        )
+    def do_action(self, valve_name: str, sccm: float, seconds: int):
+        action_rrg = self.sub_action(SetRrgSccmValueAction)
         action_rrg.action(valve_name, sccm)
 
-        action_pause = PauseAction()
-        action_pause.system = self.system
-        action_pause.action(time_pause)
+        action_pause = self.sub_action(PauseAction)
+        action_pause.action(seconds)
 
 
 class SmallPumpOutToPressureAction(AppAction):
@@ -203,21 +192,14 @@ class SmallPumpOutToPressureAction(AppAction):
     key = ACTIONS_NAMES.SMALL_PUMP_OUT_CAMERA
     args_info = [PositiveFloatKeyArgument, TimeEditArgument]
 
-    def action(self, target_pressure, max_time):
-        target_pressure = float(target_pressure)
-        max_seconds = get_total_seconds_from_time_arg(max_time)
-
+    def do_action(self, target_pressure: float, max_seconds):
         index = settings.SMALL_PUMP_INDEX
         self.system.change_pump_valve_opened(True, device_num=index)
 
-        start_time = time.time()
-
         while self.system.get_current_vakumetr_pressure() >= target_pressure:
-            # if self.get_current_recipe_state() == RECIPE_STATES.STOP:
-            if self._is_stop_state():
-                break  # TODO: Or return here?
+            self.interrupt_if_stop_state()
 
-            delta_time = time.time() - start_time
+            delta_time = time.time() - self.start_time
             if MAX_RECIPE_STEP_SECONDS and (delta_time >= MAX_RECIPE_STEP_SECONDS):
                 raise NotAchievingRecipeStepGoal
 
@@ -236,20 +218,18 @@ class BigPumpOutToPressureAction(AppAction):
     key = ACTIONS_NAMES.PUMP_OUT_CAMERA
     args_info = [PositiveFloatKeyArgument, TimeEditArgument]
 
-    def action(self, target_pressure, max_time):
+    def do_action(self, target_pressure, max_time):
         target_pressure = float(target_pressure)
         max_seconds = get_total_seconds_from_time_arg(max_time)
 
         index = settings.BIG_PUMP_INDEX
         self.system.change_pump_valve_opened(True, device_num=index)
 
-        start_time = time.time()
-
         while self.system.get_current_vakumetr_pressure() > target_pressure:
-            if self._is_stop_state():
-                break  # TODO: Or return here?
+            self.interrupt_if_stop_state()
+
             time.sleep(1)
-            delta_time = time.time() - start_time
+            delta_time = time.time() - self.start_time
             if MAX_RECIPE_STEP_SECONDS and (delta_time >= MAX_RECIPE_STEP_SECONDS):
                 raise NotAchievingRecipeStepGoal
 
@@ -265,7 +245,7 @@ class OpenValveAction(AppAction):
     key = ACTIONS_NAMES.OPEN_VALVE
     args_info = [ValveListArgument]
 
-    def action(self, valve_name):
+    def do_action(self, valve_name):
         is_gas = valve_name in list(map(lambda x: x['NAME'], settings.VALVES_CONFIGURATION))
         is_air = valve_name == settings.AIR_VALVE_CONFIGURATION['NAME']
         if is_air:
@@ -283,7 +263,7 @@ class CloseValveAction(AppAction):
     key = ACTIONS_NAMES.CLOSE_VALVE
     args_info = [ValveListArgument]
 
-    def action(self, valve_name):
+    def do_action(self, valve_name):
         is_gas = valve_name in list(map(lambda x: x['NAME'], settings.VALVES_CONFIGURATION))
         is_air = valve_name == settings.AIR_VALVE_CONFIGURATION['NAME']
         if is_air:
@@ -300,7 +280,7 @@ class CloseAllValvesAction(AppAction):
     name = TABLE_ACTIONS_NAMES.CLOSE_ALL_VALVES
     key = ACTIONS_NAMES.CLOSE_ALL_VALVES
 
-    def action(self, *args, **kwargs):
+    def do_action(self):
 
         self.system.change_air_valve_opened(False)
 
@@ -327,40 +307,34 @@ class VentilateCameraTableAction(AppAction):
     key = ACTIONS_NAMES.VENTILATE_CAMERA
     args_info = [PositiveIntKeyArgument, PositiveIntKeyArgument, PositiveFloatKeyArgument]
 
-    def action(self, x, y, target_temperature):
-        start_time = time.time()
+    def do_action(self, x, y, target_temperature):
         x = int(x)
         y = int(y)
         target_temperature = float(target_temperature)
 
         # 1 STEP
-        close_all = CloseAllValvesAction()
-        close_all.set_functions(system=self.system)
+        close_all = self.sub_action(CloseAllValvesAction)
         close_all.action()
 
         # 2 STEP
-        set_termodat_values = SetTemperatureAndSpeedAllTermodatsTableAction()
-        set_termodat_values.set_functions(system=self.system)
+        set_termodat_values = self.sub_action(SetTemperatureAndSpeedAllTermodatsTableAction)
         set_termodat_values.action(20, settings.TERMODAT_DEFAULT_SPEED)
 
         # 3 STEP
-        termodat_turn_off = TurnOffAllTermodatsTableAction()
-        termodat_turn_off.set_functions(system=self.system)
+        termodat_turn_off = self.sub_action(TurnOffAllTermodatsTableAction)
         termodat_turn_off.action()
 
         # 4 STEP
         while self.system.get_max_current_termodat_temperature() > target_temperature:
-            if self._is_stop_state():
-                return  # TODO: Or break here?
+            self.interrupt_if_stop_state()
 
             time.sleep(1)
-            delta_time = time.time() - start_time
+            delta_time = time.time() - self.start_time
             if MAX_RECIPE_STEP_SECONDS and (delta_time >= MAX_RECIPE_STEP_SECONDS):
                 raise NotAchievingRecipeStepGoal
 
         # 5 STEP
-        open_air_valve = OpenValveAction()
-        open_air_valve.set_functions(system=self.system)
+        open_air_valve = self.sub_action(OpenValveAction)
         open_air_valve.action(settings.AIR_VALVE_NAME)
 
         # 6 STEP
@@ -374,16 +348,13 @@ class VentilateCameraTableAction(AppAction):
             if (max_p / min_p - 1.0) * 100 <= x:
                 break
 
-            if self._is_stop_state():
-                break  # TODO: Or return here?
+            self.interrupt_if_stop_state()
 
-            delta_time = time.time() - start_time
-            if MAX_RECIPE_STEP_SECONDS and (delta_time >= MAX_RECIPE_STEP_SECONDS):
+            if MAX_RECIPE_STEP_SECONDS and (time.time() - self.start_time >= MAX_RECIPE_STEP_SECONDS):
                 raise NotAchievingRecipeStepGoal
 
         # 7 STEP
-        close_air_valve = CloseValveAction()
-        close_air_valve.set_functions(system=self.system)
+        close_air_valve = self.sub_action(CloseValveAction)
         close_air_valve.action(settings.AIR_VALVE_NAME)
 
 
@@ -400,22 +371,17 @@ class WaitForTemperatureAllTermodatsAction(AppAction):
     key = ACTIONS_NAMES.WAIT_TARGET_TEMPERATURE
     args_info = [PositiveIntKeyArgument, TimeEditArgument]
 
-    def action(self, target_temperature, time_limit):
-        start_time = time.time()
-
-        target_temperature = int(target_temperature)
-        max_seconds = get_total_seconds_from_time_arg(time_limit)
+    def do_action(self, target_temperature: int, max_seconds: int):
 
         # 1 STEP
         self.system.set_temperature_all_termodats_action(target_temperature)
 
         # 4 STEP
         while self.system.get_min_current_termodat_temperature() < target_temperature:
-            if self._is_stop_state():
-                return  # TODO: Or break here?
+            self.interrupt_if_stop_state()
 
             time.sleep(1)
-            delta_time = time.time() - start_time
+            delta_time = time.time() - self.start_time
             if MAX_RECIPE_STEP_SECONDS and (delta_time >= MAX_RECIPE_STEP_SECONDS):
                 raise NotAchievingRecipeStepGoal
 
@@ -435,27 +401,22 @@ class QuickShutdownAllDeviceElementsAction(AppAction):
     """
     name = TABLE_ACTIONS_NAMES.QUICK_SHUTDOWN_DEVICE
     key = ACTIONS_NAMES.QUICK_SHUTDOWN_DEVICE
-    args_info = []
 
-    def action(self):
+    def do_action(self):
         # 1 STEP: уставка печки на 20 градусов
-        set_termodat_values = SetTemperatureAndSpeedAllTermodatsTableAction()
-        set_termodat_values.set_functions(system=self.system)
+        set_termodat_values = self.sub_action(SetTemperatureAndSpeedAllTermodatsTableAction)
         set_termodat_values.action(20, settings.TERMODAT_DEFAULT_SPEED)
 
         # 2 STEP: выключить печку
-        turn_off_termodats = TurnOffAllTermodatsTableAction()
-        turn_off_termodats.set_functions(system=self.system)
+        turn_off_termodats = self.sub_action(TurnOffAllTermodatsTableAction)
         turn_off_termodats.action()
 
         # 3 STEP: закрыть все клапаны
-        close_all_valves = CloseAllValvesAction()
-        close_all_valves.set_functions(system=self.system)
+        close_all_valves = self.sub_action(CloseAllValvesAction)
         close_all_valves.action()
 
         # 4 STEP: полностью закрыть все ррг
-        close_rrg = FullCloseSingleRrgAction()
-        close_rrg.set_functions(system=self.system)
+        close_rrg = self.sub_action(FullCloseSingleRrgAction)
         for gas_name in settings.GAS_LIST:
             close_rrg.action(gas_name)
 
