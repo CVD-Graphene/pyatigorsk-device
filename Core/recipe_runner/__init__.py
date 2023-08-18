c41ba89cc6674181525e89ea5ea38a5cde758fc7
DanMitroshin <alfocintavr@mail.ru>
2023-08-18 22:45:03 +0300

    Remove deprecated set functions def

--- Core/recipe_runner/__init__.py
+++ Core/recipe_runner/__init__.py
@@ -5,5 +5,5 @@ from coregraphene.recipe import RecipeRunner
 class AppRecipeRunner(RecipeRunner):
     def _on_not_achieving_recipe_step_action(self):
         shutdown_action = QuickShutdownAllDeviceElementsAction()
-        shutdown_action.set_functions(system=self._system)
+        shutdown_action.system = self._system
         shutdown_action.action()
